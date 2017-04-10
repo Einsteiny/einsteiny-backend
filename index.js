67ba48737405baa7d954a45400e537acc0fa5f54
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2017-04-10 10:44:26 -0700

    Extend list of topics

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var request = require('request-promise');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
+var apiUrl = "https://www.khanacademy.org/api/v1/topic/";
+
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
@@ -40,7 +42,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function (req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+  res.status(200).send('Einsteiny mobile application backend!');
 });
 
 // There will be a test page available on the /test path of your server url
@@ -51,15 +53,18 @@ app.get('/test', function (req, res) {
 
 app.get('/humanities', function (req, res) {
   // create request objects
-  let apiUrl = "https://www.khanacademy.org/api/v1/topic/"
   var requests = [{ url: apiUrl + "second-empire" },
   { url: apiUrl + "realism" },
   { url: apiUrl + "post-impressionism" },
   { url: apiUrl + "impressionism" },
   { url: apiUrl + "avant-garde-sculpture" },
+  { url: apiUrl + "art-1010-ddp" },
+  { url: apiUrl + "ceramics-glass" },
+  { url: apiUrl + "sculpture" },
+  { url: apiUrl + "painting-materials-techniques" },
+  { url: apiUrl + "printmaking" },
+  { url: apiUrl + "tools-understanding-art" },
   ];
-
-
 
   Promise.map(requests, function (obj) {
     return request(obj).then(function (body) {
@@ -70,22 +75,92 @@ app.get('/humanities', function (req, res) {
     console.log("response = ", response);
     res.json(response);
   }, function (err) {
-    console.log(err);
+    console.error(err);
     // handle all your errors here
     res.json(JSON.parse(fs.readFileSync('content/humanities.json', 'utf-8')));
   });
 });
 
 app.get('/economics-finance-domain', function (req, res) {
-  res.json(JSON.parse(fs.readFileSync('content/humanities.json', 'utf-8')));
+  var requests = [
+    { url: apiUrl + "demand-curve-tutorial" },
+    { url: apiUrl + "supply-curve-tutorial" },
+    { url: apiUrl + "market-equilibrium-tutorial" },
+    { url: apiUrl + "oil-prices-tutorial" },
+    { url: apiUrl + "perfect-competition" },
+    { url: apiUrl + "monopolies-tutorial" },
+    { url: apiUrl + "monopolistic-competition-oligop" },
+    { url: apiUrl + "stocks-intro-tutorial" }
+  ];
+
+  Promise.map(requests, function (obj) {
+    return request(obj).then(function (body) {
+      return JSON.parse(body);
+    });
+  }).then(function (results) {
+    let response = JSON.parse(`{ "standalone_title": "Economics & finance", "children": ${JSON.stringify(results)} }`);
+    console.log("response = ", response);
+    res.json(response);
+  }, function (err) {
+    console.error(err);
+    // handle all your errors here
+    res.json(JSON.parse(fs.readFileSync('content/humanities.json', 'utf-8')));
+  });
 });
 
 app.get('/computing', function (req, res) {
-  res.json(JSON.parse(fs.readFileSync('content/humanities.json', 'utf-8')));
+  var requests = [{ url: apiUrl + "meet-the-computing-professional" },
+  { url: apiUrl + "internet-works-intro" },
+  { url: apiUrl + "moderninfotheory" },
+  { url: apiUrl + "modern-crypt" },
+    // { url: apiUrl + "" },
+    // { url: apiUrl + "" },
+    // { url: apiUrl + "" },
+    // { url: apiUrl + "" },
+    // { url: apiUrl + "" }
+  ];
+
+  Promise.map(requests, function (obj) {
+    return request(obj).then(function (body) {
+      return JSON.parse(body);
+    });
+  }).then(function (results) {
+    let response = JSON.parse(`{ "standalone_title": "Computing", "children": ${JSON.stringify(results)} }`);
+    console.log("response = ", response);
+    res.json(response);
+  }, function (err) {
+    console.error(err);
+    // handle all your errors here
+    res.json(JSON.parse(fs.readFileSync('content/humanities.json', 'utf-8')));
+  });
+
 });
 
 app.get('/science', function (req, res) {
-  res.json(JSON.parse(fs.readFileSync('content/humanities.json', 'utf-8')));
+  var requests = [{ url: apiUrl + "introduction-to-the-atom" },
+  { url: apiUrl + "introduction-to-compounds" },
+  { url: apiUrl + "big-bang-expansion-topic" },
+  { url: apiUrl + "intro-to-ee" },
+    // { url: apiUrl + "" },
+    // { url: apiUrl + "" },
+    // { url: apiUrl + "" },
+    // { url: apiUrl + "" },
+    // { url: apiUrl + "" }
+  ];
+
+  Promise.map(requests, function (obj) {
+    return request(obj).then(function (body) {
+      return JSON.parse(body);
+    });
+  }).then(function (results) {
+    let response = JSON.parse(`{ "standalone_title": "Science", "children": ${JSON.stringify(results)} }`);
+    console.log("response = ", response);
+    res.json(response);
+  }, function (err) {
+    console.error(err);
+    // handle all your errors here
+    res.json(JSON.parse(fs.readFileSync('content/humanities.json', 'utf-8')));
+  });
 });
 
 
