0a76cd67f72e7e946855d1b9d1fc6fdfa43081af
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2017-04-14 19:10:46 -0700

    try to fix

--- cloud/main.js
+++ cloud/main.js
@@ -1,64 +1,10 @@
 
 
-// create our job queue
-var jobs = kue.createQueue();
-
 Parse.Cloud.define('hello', function (req, res) {
   res.success('Hi');
 });
 
-Parse.Cloud.define('subscribe', function (request, response) {
-  var params = request.params;
-  var customData = params.customData;
-
-  if (!customData) {
-    response.error("Missing customData!")
-  }
-
-  var sender = JSON.parse(customData).sender;
-  var courseId = JSON.parse(customData).course;
 
 
-  // one minute
-  var minute = 60000;
-
-  var job = jobs.create('parseCloud', {
-    course: courseId
-
-  }).delay(minute * 1 * 1)
-    .priority('high')
-    .save();
-
-  job.on('complete', function () {
-    console.log(`course ${courseId} job completed`);
-  });
-
-  jobs.process('parseCloud', function (job, done) {
-    var query = new Parse.Query(Parse.Installation);
-    query.equalTo("installationId", sender);
-
-    Parse.Push.send({
-      where: query, // Set our Installation query                                                                                                                                                              
-      data: {
-        course: job.data.course
-      },
-    }, {
-        success: function () {
-
-          console.log("#### PUSH OK");
-          done();
-        }, error: function (error) {
-          console.log("#### PUSH ERROR" + error.message);
-          done();
-        }, useMasterKey: true
-      });
-
-  });
-
-  response.success('success');
-});
 
 
-// start the UI
-kue.app.listen(3000);
-console.log('UI started on port 3000');
--- index.js
+++ index.js
@@ -5,9 +5,13 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 var fs = require('fs');
+var kue = require('kue');
 
 var Promise = require("bluebird");
 var request = require('request-promise');
+
+// create our job queue
+var jobs = kue.createQueue();
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
@@ -154,3 +158,58 @@ httpServer.listen(port, function () {
 
 // This will enable the Live Query real-time server
 ParseServer.createLiveQueryServer(httpServer);
+
+Parse.Cloud.define('subscribe', function (request, response) {
+  var params = request.params;
+  var customData = params.customData;
+
+  if (!customData) {
+    response.error("Missing customData!")
+  }
+
+  var sender = JSON.parse(customData).sender;
+  var courseId = JSON.parse(customData).course;
+
+
+  // one minute
+  var minute = 60000;
+
+  var job = jobs.create('parseCloud', {
+    course: courseId
+
+  }).delay(minute * 1 * 1)
+    .priority('high')
+    .save();
+
+  job.on('complete', function () {
+    console.log(`course ${courseId} job completed`);
+  });
+
+  jobs.process('parseCloud', function (job, done) {
+    var query = new Parse.Query(Parse.Installation);
+    query.equalTo("installationId", sender);
+
+    Parse.Push.send({
+      where: query, // Set our Installation query                                                                                                                                                              
+      data: {
+        course: job.data.course
+      },
+    }, {
+        success: function () {
+
+          console.log("#### PUSH OK");
+          done();
+        }, error: function (error) {
+          console.log("#### PUSH ERROR" + error.message);
+          done();
+        }, useMasterKey: true
+      });
+
+  });
+
+  response.success('success');
+});
+
+// start the UI
+kue.app.listen(3000);
+console.log('UI started on port 3000');
