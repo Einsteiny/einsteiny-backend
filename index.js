85000991a77f6f30f2d8c6f4df9988047893222b
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2017-04-14 19:48:41 -0700

    revert functionality

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 var fs = require('fs');
+var kue = require('kue');
+var redis = require('redis');
 
 var Promise = require("bluebird");
 var request = require('request-promise');
+
+kue.redis.createClient = function() {
+    var redisUrl = url.parse(process.env.REDIS_URL)
+      , client = redis.createClient(redisUrl.port, redisUrl.hostname);
+    if (redisUrl.auth) {
+        client.auth(redisUrl.auth.split(":")[1]);
+    }
+    return client;
+};
+
+// create our job queue
+var jobs = kue.createQueue();
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
@@ -154,3 +168,56 @@ httpServer.listen(port, function () {
 
 // This will enable the Live Query real-time server
 ParseServer.createLiveQueryServer(httpServer);
+
+Parse.Cloud.define('subscribe', function (request, response) {
+  var params = request.params;
+  var customData = params.customData;
+
+  if (!customData) {
+    response.error("Missing customData!")
+  }
+
+  var sender = JSON.parse(customData).sender;
+  var courseId = JSON.parse(customData).course;
+
+
+  // one minute
+  var minute = 60000;
+
+  var job = jobs.create('parseCloud', {
+    course: courseId
+
+  }).delay(minute * 1 * 1)
+    .priority('high')
+    .save();
+
+  job.on('complete', function () {
+    console.log(`course ${courseId} job completed`);
+  });
+
+  jobs.process('parseCloud', function (job, done) {
+    var query = new Parse.Query(Parse.Installation);
+    query.equalTo("installationId", sender);
+
+    Parse.Push.send({
+      where: query, // Set our Installation query                                                                                                                                                              
+      data: {
+        course: job.data.course
+      },
+    }, {
+        success: function () {
+
+          console.log("#### PUSH OK");
+          done();
+        }, error: function (error) {
+          console.log("#### PUSH ERROR" + error.message);
+          done();
+        }, useMasterKey: true
+      });
+
+  });
+
+  response.success('success');
+});
+
+app.use(kue.app);
