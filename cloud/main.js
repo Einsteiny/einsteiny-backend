5f09c84d226ab26b52b8b7487ed3d4fcb6af4fab
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2017-04-14 22:46:05 -0700

    just try

--- cloud/main.js
+++ cloud/main.js
@@ -4,10 +4,7 @@ Parse.Cloud.define('hello', function (req, res) {
   res.success('Hi');
 });
 
-var kue = require('kue')
-var redisUrl = process.env.REDIS_URL 
-kue.createQueue({ redis: redisUrl })
-app.use('/kue', kue.app) // For the kue dashboard
+
 
 
 
--- index.js
+++ index.js
@@ -154,8 +154,60 @@ httpServer.listen(port, function () {
 ParseServer.createLiveQueryServer(httpServer);
 
 var kue = require('kue')
-var redisUrl = process.env.REDIS_URL 
-kue.createQueue({ redis: redisUrl })
+var redisUrl = process.env.REDIS_URL
+var jobs = kue.createQueue({ redis: redisUrl })
 app.use('/kue', kue.app) // For the kue dashboard
+
+
+Parse.Cloud.define('subscribe', function (request, response) {
+  var params = request.params;
+  var customData = params.customData;
+
+  if (!customData) {
+    response.error("Missing customData!")
+  }
+
+  var sender = JSON.parse(customData).sender;
+  var courseId = JSON.parse(customData).course;
+
+
+  // one minute
+  var minute = 60000;
+
+  var job = jobs.create('parseCloud', {
+    course: courseId
+
+  }).delay(minute * 1 * 1)
+    .priority('high')
+    .save();
+
+  job.on('complete', function () {
+    console.log(`course ${courseId} job completed`);
+  });
+
+  jobs.process('parseCloud', function (job, done) {
+    var query = new Parse.Query(Parse.Installation);
+    query.equalTo("installationId", sender);
+
+    Parse.Push.send({
+      where: query, // Set our Installation query                                                                                                                                                              
+      data: {
+        course: job.data.course
+      },
+    }, {
+        success: function () {
+
+          console.log("#### PUSH OK");
+          done();
+        }, error: function (error) {
+          console.log("#### PUSH ERROR" + error.message);
+          done();
+        }, useMasterKey: true
+      });
+
+  });
+
+  response.success('success');
+});
 
 
