6cacc2d701ae934d46b584b69dc776dd2cb77952
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2017-04-14 17:17:07 -0700

    add function ping reply

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,28 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('pingReply', function(request, response) {
+  var params = request.params;
+  var customData = params.customData;
+
+  if (!customData) {
+    response.error("Missing customData!")
+  }
+
+  var sender = JSON.parse(customData).sender;
+  var query = new Parse.Query(Parse.Installation);
+  query.equalTo("installationId", sender);
+
+  Parse.Push.send({
+  where: query,
+  // Parse.Push requires a dictionary, not a string.
+  data: {"alert": "The Giants scored!"},
+  }, { success: function() {
+     console.log("#### PUSH OK");
+  }, error: function(error) {
+     console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
+
+  response.success('success');
+});
