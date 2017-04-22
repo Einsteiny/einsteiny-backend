59e48fec68a0950dcb3f850916c0856266adff6e
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2017-04-21 20:08:30 -0700

    change pic for meet professional

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ let images = ["second-empire.jpg", "realism.jpg", "impressionism.jpg", "post-imp
   "painting-materials-techniques.jpg", "printmaking.jpg", "demand-curve-tutorial.jpg", "supply-curve-tutorial.jpg",
    "market-equilibrium-tutorial.jpg", "oil-prices-tutorial.jpg", "perfect-competition.jpg", "monopolies-tutorial.jpg", 
    "monopolistic-competition-oligop.jpg", "stocks-intro-tutorial.jpg",
-   "meet-the-computing-professional.jpeg", "internet-works-intro.jpg", "moderninfotheory.jpg", "modern-crypt.jpg",
+   "meet-the-computing-professional.jpg", "internet-works-intro.jpg", "moderninfotheory.jpg", "modern-crypt.jpg",
    "introduction-to-the-atom.jpeg", "introduction-to-compounds.png", "big-bang-expansion-topic.jpg", "intro-to-ee.jpg"
    
    ];
