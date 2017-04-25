ad063717f9480cbad8eb728272ecfa8503270119
Antonina Cherednichenko <antonina.cherednichenko@gmail.com>
2017-04-25 14:35:26 -0700

    add facebook id

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  },
+  auth: {
+   facebook: {
+     appIds: process.env.FACEBOOK_APP_ID
+   }
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
